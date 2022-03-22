d65cdfe3dded4a0213a1211e08b229cced9cc9c3
FrederikReigstad <Frederik9876@gmail.com>
2022-03-22 14:33:50 +0100

    Exsasice 1 to 3 done
    Exercise 1
    Create a new Java console project (New > Project > Java) with the name MovieSortingProject.
    In your new project, implement a Movie class with the following properties:
    
    int id,
    String title,
    String director,
    String genre,
    int productionYear,
    Double rating
    
    Add a constructor, which takes in all the properties as parameters
    Add getters and setters for all properties (id should be read-only).
    Override the toString() method for the class, so it returns a nicely formatted String containing the property values.
    
    Exercise 2
    In the class with main(), add a list of movies containing at least 5 different Movie objects. Go to www.imdb.com if you need inspiration :)
    Shuffle the list (use the Collections.shuffle() method), and print out the list content to the screen.
    
    Exercise 3
    Try to use the Collections.sort() method to sort the Movie objects in the list.
    What happens?
    
    Implement the Comparable<Movie> interface in the Movie class. Implement the needed compareTo method, so it compares Movie instances by id numbers.
    
                          public int compareTo(Movie m)
                          {
                              return this.getId() – m.getId();
                          }
    
    Is the sorting working correctly now?
    Modify your compareTo method to compare on the productionYear property instead. Re-test.

--- scrMovies/src/Main.java
+++ scrMovies/src/Main.java
@@ -6,48 +6,59 @@ public class Main {
     public static void main(String[] args) {
         //TODo code appliction
 
-        Car c1 = new Car("VW", "Golf GTE", "blue", 215);
-        Car c2 = new Car("Peugeot", "308 SW", "reaalyy green", 207);
-        Car c3 = new Car("BMW", "320i", "blak", 212);
-        Car c4 = new Car("Tesla", "Model s", "red", 250);
+        Movies movies1 = new Movies(31, "Scary Movie", "Keenen Ivory Wayans", "Horror",2000, 8.2);
+        Movies movies2 = new Movies(52, "Scary Movie 2", "Keenen Ivory Wayans", "Horror",2001, 8.9);
+        Movies movies3 = new Movies(333, "Scary Movie 3", "David Zucker", "Horror",2003, 8.6);
+        Movies movies4 = new Movies(534, "Scary Movie 4", "David Zucker", "Horror",2006, 8.1);
+        Movies movies5 = new Movies(725, "Scary Movie 5", "Malcolm D. Lee", "Horror",2013, 8.7);
+        Movies movies6 = new Movies(12, "Die Hard", "John McTiernan", "Action",1988, 10.0);
+        Movies movies7 = new Movies(21, "Die Hard 2 Die Harder", "Renny Harlin", "Action",1990, 9.5);
+        Movies movies8 = new Movies(28, "Die Hard Mega Hard", "John McTiernan", "Action",1995, 9.8);
 
-        ArrayList<Car> cars = new ArrayList<>();
+        ArrayList<Movies> movies = new ArrayList<>();
 
-        cars.add(c1);
-        cars.add(c2);
-        cars.add(c3);
-        cars.add(c4);
+        movies.add(movies1);
+        movies.add(movies2);
+        movies.add(movies3);
+        movies.add(movies4);
+        movies.add(movies5);
+        movies.add(movies6);
+        movies.add(movies7);
+        movies.add(movies8);
 
 
         System.out.println("Before sort....");
 
-        for (Car c : cars) {
+        Collections.shuffle(movies);
+
+        for (Movies c : movies) {
             System.out.println(c);
+
 
         }
 
         //java - Lambda Expression
-        Comparator<Car> byBrand = (Car car1, Car car2) -> car1.getBrand().compareTo(car2.getBrand());
-        Comparator<Car> byColor = (Car car1, Car car2) -> car1.getColor().compareTo(car2.getColor());
-        Comparator<Car> byTopSpeed = (Car car1, Car car2) -> {
-          /*  if (car1.getTopSpeed() < car2.getTopSpeed()) {
-                return -1;
-            } else if (car1.getTopSpeed() > car2.getTopSpeed()) {
-                return 1;
-            } else {
-                return 0;
-            } */
-            return car1.getTopSpeed() - car2.getTopSpeed();
+        Comparator<Movies> byDirector = (Movies movie1, Movies movie2) -> movie1.getDirector().compareTo(movie2.getDirector());
+        Comparator<Movies> byGenre = (Movies movie1, Movies movie2) -> movie1.getGenre().compareTo(movie2.getGenre());
+        Comparator<Movies> byTitle = (Movies movie1, Movies movie2) -> movie1.getTitle().compareTo(movie2.getTitle());
+        Comparator<Movies> byid = (Movies movie1, Movies movie2) -> {
+            return movie1.getId() - movie1.getId();
         };
 
 
+       /* Comparator<Movies> byRating = (Movies movie1, Movies movie2) -> {
+            //return movie1.getRating() - movie1.getRating();
+        };*/
 
-        Collections.sort(cars, byTopSpeed);
+
+
+
+        Collections.sort(movies, byTitle);
 
 
         System.out.println("After sort....");
 
-        for (Car c : cars)
+        for (Movies c : movies)
             System.out.println(c);
 
 
