74840aaecbb3256ffed6356cf53d920365082785
FrederikReigstad <Frederik9876@gmail.com>
2022-03-22 14:11:37 +0100

    Top speed sorting

--- src/src/Main.java
+++ src/src/Main.java
@@ -31,8 +31,20 @@ public class Main {
         //java - Lambda Expression
         Comparator<Car> byBrand = (Car car1, Car car2) -> car1.getBrand().compareTo(car2.getBrand());
         Comparator<Car> byColor = (Car car1, Car car2) -> car1.getColor().compareTo(car2.getColor());
+        Comparator<Car> byTopSpeed = (Car car1, Car car2) -> {
+          /*  if (car1.getTopSpeed() < car2.getTopSpeed()) {
+                return -1;
+            } else if (car1.getTopSpeed() > car2.getTopSpeed()) {
+                return 1;
+            } else {
+                return 0;
+            } */
+            return car1.getTopSpeed() - car2.getTopSpeed();
+        };
 
-        Collections.sort(cars, byColor);
+
+
+        Collections.sort(cars, byTopSpeed);
 
 
         System.out.println("After sort....");
