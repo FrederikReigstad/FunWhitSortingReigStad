141dd435f10b48c48c040004a2642ed47081c622
FrederikReigstad <Frederik9876@gmail.com>
2022-03-22 13:54:45 +0100

    All new code
    kode der sotere biler med biler hard codet i programmet

---
+++ src/src/Main.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //TODo code appliction
+
+        Car c1 = new Car("VW", "Golf GTE", "blue", 215);
+        Car c2 = new Car("Peugeot", "308 SW", "reaalyy green", 207);
+        Car c3 = new Car("BMW", "320i", "blak", 212);
+        Car c4 = new Car("Tesla", "Model s", "red", 250);
+
+        ArrayList<Car> cars = new ArrayList<>();
+
+        cars.add(c1);
+        cars.add(c2);
+        cars.add(c3);
+        cars.add(c4);
+
+
+        System.out.println("Before sort....");
+
+        for (Car c : cars) {
+            System.out.println(c);
+
+        }
+
+        //java - Lambda Expression
+        Comparator<Car> byBrand = (Car car1, Car car2) -> car1.getBrand().compareTo(car2.getBrand());
+        Comparator<Car> byColor = (Car car1, Car car2) -> car1.getColor().compareTo(car2.getColor());
+
+        Collections.sort(cars, byColor);
+
+
+        System.out.println("After sort....");
+
+        for (Car c : cars)
+            System.out.println(c);
+
+
+
+
+
+    }
+}
